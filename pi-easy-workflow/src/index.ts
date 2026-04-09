1c1c50d94c2eec8e742a64430757636e627b0bf9
Jardel G. Marceno <jmarceno@gmail.com>
2026-04-09 12:11:37 -0300

    (chore) scaffold pi-easy-workflow project basics

---
+++ pi-easy-workflow/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Pi Easy Workflow entrypoint (scaffolding stub).
+ * TODO: Replace with Pi runtime/server bootstrap in migration tasks.
+ */
+export function main(): void {
+  console.log("[pi-easy-workflow] Scaffolding ready. Runtime not implemented yet.")
+}
+
+main()
